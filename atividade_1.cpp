f1f273376c28e1984e7eeb45cbaa56eab89668ca
JonathanSeverino <jonathandesouza046@gmail.com>
2021-05-21 16:01:44 -0300

    funcionando! Mas precisa de uns retoques...rsrsr

--- atividade_1.cpp
+++ atividade_1.cpp
@@ -23,17 +23,33 @@ int total_trocas(char* v_palavra, char letra_qvs){
 
 
 
-//criando função: Ponteiro para vetor palavra, um inteiro para o tamanho
-//e outro ponteiro para a nova palavra
+//Função que efetua a troca das letras, usando vetores como parametros
+
+char *Troca_letra (char *vet_palavra, char *palavra_nova, char letra_s, char letra_e){
+
+    int tam = strlen(vet_palavra);
+        
+    for(int i=0; i < tam; i++){
+        if(letra_s == vet_palavra[i]){
+            vet_palavra[i] = letra_e;
+            palavra_nova = vet_palavra;
+        }
+    }
+
+    return palavra_nova;
+
+}
 
 
 int main(){
     
+    //Declarando variaveis
     char* palavra = new char[100];
     char* nova_palavra = new char[100];
     char l_sai, l_entra;
+    int total = 0;
 
-    
+    //Interação com o usuário
     cout << "Informe sua palavra: " << endl;
     cin >> palavra;
 
@@ -45,7 +61,12 @@ int main(){
 
     cout << endl;
 
-    cout << "Total de trocas efetuadas: " << total_trocas(palavra, l_sai) << endl;
+    //Pegando o total de trocas e imprimindo os resultados
+
+    total = total_trocas(palavra, l_sai);
+    cout << palavra << endl;   
+    cout << Troca_letra(palavra, nova_palavra, l_sai, l_entra) << endl;    
+    cout << "Total de trocas efetuadas: " << total << endl;  
 
 
 
